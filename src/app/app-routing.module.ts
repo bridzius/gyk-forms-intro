cde0b53836702efb233f081213259589106ee5b1
minijus <3633549+minijus@users.noreply.github.com>
2022-11-28 21:17:34 +0200

    add routing for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomePageComponent } from "./components/home-page.component";
+import { NewsPageComponent } from "./components/news-page.component";
+import { MoviesPageComponent } from "./components/movies-page.component";
+import { TvShowsPageComponent } from "./components/tv-shows-page.component";
+import { CelebsPageComponent } from "./components/celebs-page.component";
 
 const routes: Routes = [
-
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", component: HomePageComponent },
+  { path: "news", component: NewsPageComponent },
+  { path: "movies", component: MoviesPageComponent },
+  { path: "tv-shows", component: TvShowsPageComponent },
+  { path: "celebs", component: CelebsPageComponent },
+  { path: "**", redirectTo: "/home" }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
