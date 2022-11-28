f914d00c02d752fdedcbc8d990666cb49ae1877d
minijus <3633549+minijus@users.noreply.github.com>
2022-11-28 21:17:34 +0200

    add page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page.component';
+import { NewsPageComponent } from './components/news-page.component';
+import { MoviesPageComponent } from './components/movies-page.component';
+import { TvShowsPageComponent } from './components/tv-shows-page.component';
+import { CelebsPageComponent } from './components/celebs-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NewsPageComponent,
+    MoviesPageComponent,
+    TvShowsPageComponent,
+    CelebsPageComponent
   ],
   imports: [
     BrowserModule,
