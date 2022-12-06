161b88ac9f4f553c0caa483efc2d4ff39ded5f78
Jurgis Bridzius <jurgis@bridzius.lt>
2022-12-06 20:19:16 +0200

    task-3 done

--- src/app/components/movie-search-form/movie-search-form.component.ts
+++ src/app/components/movie-search-form/movie-search-form.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+interface MovieSearchForm {
+  title: FormControl<string | null>;
+  year: FormControl<number | null>;
+}
 
 @Component({
   selector: 'app-movie-search-form',
   templateUrl: './movie-search-form.component.html',
-  styleUrls: ['./movie-search-form.component.scss']
+  styleUrls: ['./movie-search-form.component.scss'],
 })
 export class MovieSearchFormComponent {
+  public movieSearchForm: FormGroup<MovieSearchForm>;
+  constructor(formBuilder: FormBuilder) {
+    this.movieSearchForm = formBuilder.group({
+      title: formBuilder.control<string>(''),
+      year: formBuilder.control<number | null>(null),
+    });
+  }
 
+  findMovie() {
+    console.log(this.movieSearchForm.value);
+  }
 }
