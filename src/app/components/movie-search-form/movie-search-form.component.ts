483580afb8281b8379dec692a0da0a6b3aaa4495
Jurgis Bridzius <jurgis@bridzius.lt>
2022-12-06 19:54:38 +0200

    tas-2 done

---
+++ src/app/components/movie-search-form/movie-search-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-search-form',
+  templateUrl: './movie-search-form.component.html',
+  styleUrls: ['./movie-search-form.component.scss']
+})
+export class MovieSearchFormComponent {
+
+}
